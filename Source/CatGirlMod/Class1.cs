a29a1dd2037fb03e5aab39340a10fadf1ab82aca
Zachary Hankin <zacharyjameshankin@gmail.com>
2022-05-29 20:08:54 +1200

    Initial commit

---
+++ Source/CatGirlMod/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+
+namespace TestMod
+{
+    [StaticConstructorOnStartup]
+    public static class MyMod
+    {
+        static MyMod() //our constructor
+        {
+            Log.Message("Hello World!TEST"); //Outputs "Hello World!" to the dev console.
+            Log.Message("Hello World!TEST"); //Outputs "Hello World!" to the dev console.
+            Log.Message("Hello World!TEST"); //Outputs "Hello World!" to the dev console.
+            Log.Message("Hello World!TEST"); //Outputs "Hello World!" to the dev console.
+            Log.Message("Hello World!TEST"); //Outputs "Hello World!" to the dev console.
+            Log.Message("Hello World!TEST"); //Outputs "Hello World!" to the dev console.
+        }
+    }
+}
